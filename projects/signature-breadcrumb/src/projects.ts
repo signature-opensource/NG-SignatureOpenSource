9096a9db07aa42901a8041f8da99e131c06c48c6
Arthur <arthur.cheng@signature-code.com>
2020-07-30 11:47:32 +0200

    fixed repository's file hierarchy

--- projects/signature-breadcrumb/src/projects.ts
+++ projects/signature-breadcrumb/src/projects.ts
@@ -2,6 +2,6 @@
  * Public API Surface of signature-breadcrumb
  */
 
-export * from './lib/signature-breadcrumb.service';
+export * from './lib/breadcrumb.service';
 export * from './lib/signature-breadcrumb.component';
-export * from './lib/signature-breadcrumb.module';
+export * from './lib/breadcrumb.module';
