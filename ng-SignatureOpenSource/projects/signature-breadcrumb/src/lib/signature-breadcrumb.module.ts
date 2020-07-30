0b2fe5e093de940a2d38dca59ca51d7e60b69db5
Arthur <arthur.cheng@signature-code.com>
2020-07-30 10:46:44 +0200

    angular project initialize + create signature-breadcrumb library

---
+++ ng-SignatureOpenSource/projects/signature-breadcrumb/src/lib/signature-breadcrumb.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SignatureBreadcrumbComponent } from './signature-breadcrumb.component';
+
+
+
+@NgModule({
+  declarations: [SignatureBreadcrumbComponent],
+  imports: [
+  ],
+  exports: [SignatureBreadcrumbComponent]
+})
+export class SignatureBreadcrumbModule { }
---
+++ ng-SignatureOpenSource/projects/signature-breadcrumb/src/projects.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of signature-breadcrumb
+ */
+
+export * from './lib/signature-breadcrumb.service';
+export * from './lib/signature-breadcrumb.component';
+export * from './lib/signature-breadcrumb.module';
