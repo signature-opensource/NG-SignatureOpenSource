9b35c2b2925ee15e9c7203df06110f45edc3885e
Arthur <arthur.cheng@signature-code.com>
2020-07-30 10:52:00 +0200

    update angular to 10.0.6, and generate signature-router-history library

---
+++ ng-SignatureOpenSource/projects/signature-router-history/src/projects.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of signature-router-history
+ */
+
+export * from './lib/signature-router-history.service';
+export * from './lib/signature-router-history.component';
+export * from './lib/signature-router-history.module';
